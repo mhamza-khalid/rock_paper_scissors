e808cea7be7ae928ad82e7ac4fef0ed597adeee2
Muhammad Hamza Khalid <mkhalid.bee19seecs@seecs.edu.pk>
2023-06-19 21:52:42 +0500

    Added modified files rps.html, rps.js and style.css

--- rps.js
+++ rps.js
@@ -1,86 +1,217 @@
+let comp_count = 0;
+let player_count = 0;
+let count = 0;
 
-function game(){
-    let comp_count = 0;
-    let player_count = 0;
-    for(let i=0 ; i<5 ; i++){    // plays game 5 times
-       let computerChoice = getComputerChoice(); //genrates computers choice
+const result1 = document.querySelector('.result');
+const declare1 = document.querySelector('.declare')
+const urScore = document.querySelector('.you');
+const compScore = document.querySelector('.computer');
 
-       function getComputerChoice(){
-           let string1 = "Rock";
-           let string2 = "Paper";
-           let string3 = "Scissors";
-     
-           let x = Math.floor(Math.random() * 3)
-     
-           if (x===0){
-             return string1;
-           }
-           else if (x===1){
-             return string2;
-           }
-           else{
-             return string3;
-           }
-     }
-       console.log(computerChoice);  // prints computers choice on console
+const rock1 = document.querySelector('.rock');
+const paper1 = document.querySelector('.paper');
+const scissors1 = document.querySelector('.scissors');
 
-       let playerSelection1 = prompt("Type your choice\nRock or Paper or Scissors"); //prompt for user to enter choice
 
-       let playerSelection = playerSelection1.toLowerCase(); 
-       
-       playGame(computerChoice, playerSelection);
-       
-       function playGame (computer, human){
-           if ( computer == "Rock" && human == "scissors"){
-               comp_count = comp_count+1;
-               alert("You lose!" + " " + computer  + " " + "beats" + " " + human 
-                + "\n" + "Computer :" + " " + comp_count + "\n" + "You :" + " " + player_count );
-               
-           }
-           else if( computer == "Rock" && human == "paper"){
-               player_count = player_count + 1;
-               alert("You win!" + " " + human + " " + "beats" + " " + computer
-               + "\n" + "Computer :" + " " + comp_count + "\n"+ "You :" + " " + player_count );
-               
-           }
-           else if( computer == "Paper" && human == "scissors"){
-               player_count = player_count + 1;
-               alert("You win!" + " " + human + " " + "beats" + " " + computer
-               + "\n" + "Computer :" + " " + comp_count + "\n"+ "You :" + " " + player_count );
-               
-           }
-           else if( computer == "Paper" && human == "rock"){
-               comp_count = comp_count+1;
-               alert("You lose!" + " " + computer  + " " + "beats" + " " + human
-               + "\n" + "Computer :" + " " + comp_count + "\n"+ "You :" + " " + player_count );
-               
-           }
-           else if( computer == "Scissors" && human == "paper"){
-               comp_count = comp_count+1;
-               alert("You lose!" + " " + computer  + " " + "beats" + " " + human
-               + "\n" + "Computer :" + " " + comp_count + "\n"+ "You :" + " " + player_count );
-               
-           }
-           else if( computer == "Scissors" && human == "rock"){
-               player_count = player_count + 1;
-               alert(`You win! ${human} beats ${computer}\nComputer : ${comp_count}\nYou : ${player_count}` );
-               
-           }
-           else{
-               alert("Game Tie!")
-           }
-       }
+
+
+
+rock1.addEventListener('click', function (event) {
+    if (event.button === 0) {
+        count = count + 1;
+        let computerChoice = getComputerChoice(); //genrates computers choice
+        while (count == 5){
+            if (comp_count > player_count){
+                result1.textContent = "Computer wins the best of 5 game!";
+                comp_count = 0;
+                player_count = 0;
+                count = 0 ;
+            }
+            else if (count <=5){
+                result1.textContent = "You win the best of 5 game!";
+                comp_count = 0;
+                player_count = 0;
+                count = 0 ;
+            }
+        }
+        function getComputerChoice() {
+            let string1 = "Rock";
+            let string2 = "Paper";
+            let string3 = "Scissors";
+
+            let x = Math.floor(Math.random() * 3)
+
+            if (x === 0) {
+                return string1;
+            }
+            else if (x === 1) {
+                return string2;
+            }
+            else {
+                return string3;
+            }
+        }
+        playGame(computerChoice, "rock");
+
+        function playGame(computer, human) {
+
+            if (computer == "Paper" && human == "rock") {
+                comp_count = comp_count + 1;
+                declare1.textContent = "You lose! Paper Beats Rock!";
+                urScore.textContent = `You : ${player_count}`;
+                compScore.textContent = `Computer : ${comp_count}`;
+            }
+            else if (computer == "Scissors" && human == "rock") {
+                player_count = player_count + 1;
+                declare1.textContent= "You win! Rock Beats Scissors!";
+                urScore.textContent = `You : ${player_count}`;
+                compScore.textContent = `Computer : ${comp_count}`;
+
+            }
+            else {
+                declare1.textContent = "Game Tie!";
+                urScore.textContent = `You : ${player_count}`;
+                compScore.textContent = `Computer : ${comp_count}`;
+            }
+
+            while (compScore == 5){
+                result1.textContent = "Computer wins the best of 5 game!";
+                comp_count = 0;
+                player_count = 0;
+            }
+        }
     }
-    if (player_count > comp_count){
-       alert("You win the best of 5 game!")
+});
+
+paper1.addEventListener('click', function (event) {
+    if (event.button === 0) {
+        count = count + 1;
+        let computerChoice = getComputerChoice(); //genrates computers choice
+        while (count ==5){
+            if (comp_count > player_count){
+                result1.textContent = "Computer wins the best of 5 game!";
+                comp_count = 0;
+                player_count = 0;
+                count = 0 ;
+            }
+            else if (count <=5){
+                result1.textContent = "You win the best of 5 game!";
+                comp_count = 0;
+                player_count = 0;
+                count = 0 ;
+            }
+        }
+        function getComputerChoice() {
+            let string1 = "Rock";
+            let string2 = "Paper";
+            let string3 = "Scissors";
+
+            let x = Math.floor(Math.random() * 3)
+
+            if (x === 0) {
+                return string1;
+            }
+            else if (x === 1) {
+                return string2;
+            }
+            else {
+                return string3;
+            }
+        }
+        playGame(computerChoice, "paper");
+
+        function playGame(computer, human) {
+
+            if (computer == "Scissors" && human == "paper") {
+                comp_count = comp_count + 1;
+                declare1.textContent = "You lose! Scissors Beats Paper !";
+                urScore.textContent = `You : ${player_count}`;
+                compScore.textContent = `Computer : ${comp_count}`;
+            }
+            else if (computer == "Rock" && human == "paper") {
+                player_count = player_count + 1;
+                declare1.textContent= "You win! Paper Beats Rock!";
+                urScore.textContent = `You : ${player_count}`;
+                compScore.textContent = `Computer : ${comp_count}`;
+
+            }
+            else {
+                declare1.textContent = "Game Tie!";
+                urScore.textContent = `You : ${player_count}`;
+                compScore.textContent = `Computer : ${comp_count}`;
+            }
+        }
     }
-    else{
-       alert("You lost the best of 5 game!")
+});
+
+
+scissors1.addEventListener('click', function (event) {
+    if (event.button === 0) {
+        console.log("Hello");
+        count = count + 1;
+        let computerChoice = getComputerChoice(); //genrates computers choice
+        while (count == 5){
+            if (comp_count > player_count){
+                result1.textContent = "Computer wins the best of 5 game!";
+                comp_count = 0;
+                player_count = 0;
+                count = 0 ;
+            }
+            else if (count <=5){
+                result1.textContent = "You win the best of 5 game!";
+                comp_count = 0;
+                player_count = 0;
+                count = 0 ;
+            }
+        }
+        function getComputerChoice() {
+            let string1 = "Rock";
+            let string2 = "Paper";
+            let string3 = "Scissors";
+
+            let x = Math.floor(Math.random() * 3)
+
+            if (x === 0) {
+                return string1;
+            }
+            else if (x === 1) {
+                return string2;
+            }if (comp_count == 5){
+                result1.textContent = "Computer wins the best of 5 game!"
+                comp_count = 0;
+                player_count = 0;
+            }
+            
+            if (player_count == 5){
+                result1.textContent = "You win the best of 5 game!"
+                comp_count = 0;
+                player_count = 0;
+            }
+            else {
+                return string3;
+            }
+        }
+        playGame(computerChoice, "scissors");
+
+        function playGame(computer, human) {
+
+            if (computer == "Rock" && human == "scissors") {
+                comp_count = comp_count + 1;
+                declare1.textContent = "You lose! Rock Beats Scissors!";
+                urScore.textContent = `You : ${player_count}`;
+                compScore.textContent = `Computer : ${comp_count}`;
+            }
+            else if (computer == "Paper" && human == "Scissors") {
+                player_count = player_count + 1;
+                declare1.textContent= "You win! Scissors Beats Paper!";
+                urScore.textContent = `You : ${player_count}`;
+                compScore.textContent = `Computer : ${comp_count}`;
+
+            }
+            else {
+                declare1.textContent = "Game Tie!";
+                urScore.textContent = `You : ${player_count}`;
+                compScore.textContent = `Computer : ${comp_count}`;
+            }
+        }
     }
-}
-
-game(); 
-
-
-
-
+});
